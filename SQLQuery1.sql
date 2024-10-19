ad131fb7f7545ee1daab4cc8be47bf4cd7bae9d0
MuditMathur72 <127241368+muditmathur7@users.noreply.github.com>
2024-10-20 03:25:50 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,19 @@
+create database HerWatch;
+use HerWatch;
+create table user_details (
+Email_id varchar(100) not null UNIQUE,
+First_name varchar(100) not null,
+Last_name varchar(100),
+Age int not null,
+Phone_number varchar(100) primary key,
+username varchar(100) UNIQUE,
+password varchar(100) not null,
+pin int not null
+);
+
+exec sp_help 'user_details';
+Select * from user_details;
+insert into user_details values('pranatikackar2626@gmail.com','Pranati','Kackar',20,8005917713,'Pranati26','pranu',1234);
+insert into user_details values('mvyas1302@gmail.com','Mayank','Vyas',21,9370664741,'Mayank13','mayank',1234);
+delete from user_details where Age=20;
+select * from user_details order by Age desc;
